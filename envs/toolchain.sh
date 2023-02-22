3bc9cb108eb147417e286e15e0a1fb097e9b0da9
ZhaoWei Xu <paldier@hotmail.com>
2023-02-22 15:36:45 +0800

    update toolchain script

--- envs/toolchain.sh
+++ envs/toolchain.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 ln -sf ~/mtk-toolchains/toolchain-aarch64_cortex-a53_gcc-8.4.0_musl /opt/
+ln -sf ~/mtk-toolchains/toolchain-mipsel_24kc_gcc-5.4.0_musl-1.1.24 /opt/
 ln -sf ~/qca-toolchains/openwrt-gcc520_musl.arm /opt/
+
