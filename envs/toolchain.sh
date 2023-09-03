5840647ca6e53f12600d362f67469f42ec866634
ZhaoWei Xu <paldier@hotmail.com>
2023-09-03 08:34:51 +0800

    update toolchain.sh

--- envs/toolchain.sh
+++ envs/toolchain.sh
@@ -4,4 +4,5 @@ ln -sf ~/mtk-toolchains/toolchain-aarch64_cortex-a53_gcc-8.4.0_musl /opt/
 ln -sf ~/mtk-toolchains/toolchain-mipsel_24kc_gcc-5.4.0_musl-1.1.24 /opt/
 ln -sf ~/qca-toolchains/openwrt-gcc520_musl.arm /opt/
 ln -sf ~/qca-toolchains/openwrt-gcc520_musl.aarch64 /opt/
+ln -sf ~/qca-toolchains/toolchain-aarch64_cortex-a53+neon-vfpv4_gcc-8.3.0_musl /opt/
 
